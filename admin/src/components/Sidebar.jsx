a970c32a77767c722800d3f7e5653e8c1e78f99d
AnisJshK <anisshaikh2131@gmail.com>
2026-08-05 14:23:21 +0530

    Admin-add-items-v1

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -6,19 +6,19 @@ const Sidebar = () => {
   return (
       <div className="w-[18%] min-h-screen  border-r-2 border-zinc-200 " >
             <div className="flex flex-col gap-4 pt-6 pl-[20%] text-[15px]">
-            <NavLink className={"flex items-center gap-3 border border-r-0 px-3 py-2 rounded-l  border-zinc-300 text-zinc-800 hover:bg-zinc-900 hover:text-white text-xs font-semibold uppercase tracking-wider rounded-2xl transition-colors duration-700"} to={"/add"}>
+            <NavLink className={"flex items-center gap-3 border border-r-0 px-3 py-2 rounded-l  border-zinc-300 text-zinc-800 hover:bg-green-600 hover:text-white text-xs font-semibold uppercase tracking-wider rounded-2xl transition-colors duration-700"} to={"/add"}>
                 {/* <img className="w-5 h-5" src={assets.add_icon} alt="" /> */}
                 <Plus/>
                 
                 <p className="hidden md:block">Add Items</p>
             </NavLink>
-            <NavLink className={"flex items-center gap-3 border border-r-0 px-3 py-2 rounded-l  border-zinc-300 text-zinc-800 hover:bg-zinc-900 hover:text-white text-xs font-semibold uppercase tracking-wider rounded-2xl transition-colors duration-700"} to={"/list"}>
+            <NavLink className={"flex items-center gap-3 border border-r-0 px-3 py-2 rounded-l  border-zinc-300 text-zinc-800 hover:bg-green-600 hover:text-white text-xs font-semibold uppercase tracking-wider rounded-2xl transition-colors duration-700"} to={"/list"}>
                  {/* <img className="w-5 h-5" src={assets.order_icon} alt="" /> */}
                  <ListIcon/>
                 
                 <p className="hidden md:block">List Items</p>
             </NavLink>
-            <NavLink className={"flex items-center gap-3 border border-r-0 px-3 py-2 rounded-l  border-zinc-300 text-zinc-800 hover:bg-zinc-900 hover:text-white text-xs font-semibold uppercase tracking-wider rounded-2xl transition-colors duration-700"} to={"/orders"}>
+            <NavLink className={"flex items-center gap-3 border border-r-0 px-3 py-2 rounded-l  border-zinc-300 text-zinc-800 hover:bg-green-600 hover:text-white text-xs font-semibold uppercase tracking-wider rounded-2xl transition-colors duration-700"} to={"/orders"}>
                 {/* <img className="w-5 h-5" src={assets.order_icon} alt="" />
                 <Plus/> */}
                 <ListChecksIcon/>
--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,107 @@
+import { assets } from "../assets/admin_assets/assets"
 
 const Add = () => {
   return (
-    <div>Add</div>
+   <form action="" className="flex flex-col w-full items-start gap-3">
+    <div>
+      <p className="mb-2">Upload Image</p>
+
+      <div className="flex gap-2">
+        <label htmlFor="image1">
+          <img className="w-20" src={assets.upload_area} alt="" />
+          <input  type="file" id="image1" hidden  />
+        </label>
+        <label htmlFor="image2">
+          <img className="w-20" src={assets.upload_area} alt="" />
+          <input  type="file" id="image2" hidden  />
+        </label>
+        <label htmlFor="image3">
+          <img className="w-20" src={assets.upload_area} alt="" />
+          <input  type="file" id="image3" hidden  />
+        </label>
+        <label htmlFor="image4">
+          <img className="w-20" src={assets.upload_area} alt="" />
+          <input  type="file" id="image4" hidden  />
+        </label>
+      </div>
+
+    </div>
+
+    <div className="w-full">
+      <p>
+        Product name
+      </p>
+      <input className="w-full max-w-125 px-3 py-2" type="text" placeholder="Type here" required />
+    </div>
+
+    <div className="w-full">
+      <p className="mb-2">
+        Product Description
+      </p>
+      <textarea className="w-full max-w-125 px-3 py-2" type="text" placeholder="Write content here" required />
+    </div>
+    
+    <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8 items-center">
+
+      <div>
+        <p className="mb-2"> Product Category</p>
+        <select className="px-3 py-2 w-full">
+          <option value="Men">Men</option>
+          <option value="Women">Women</option>
+          <option value="Kids">Kids</option>
+        </select>
+      </div>
+
+      <div>
+        <p className="mb-2" >Sub Category</p>
+        <select className="px-3 py-2 w-full">
+          <option value="Topwear">Topwear</option>
+          <option value="Bottomwear">Bottomwear</option>
+          <option value="Winterwear">WinterWear</option>
+        </select>
+      </div>
+
+      <div>
+      <p className="mb-2">Product Price</p>
+      <input className="w-full px-3 py-2 sm:w-30 scroll-off" type="Number" placeholder="25" />
+    </div>
+
+    </div>
+
+    <div>
+      <p className="mb-2">
+        Product Sizes
+      </p>
+      <div className="flex gap-3">
+        <div>
+          <p className="bg-slate-200 px-3 py-1 cursor-pointer">S</p>
+        </div>
+        <div>
+          <p className="bg-slate-200 px-3 py-1 cursor-pointer">M</p>
+        </div>
+        <div>
+          <p className="bg-slate-200 px-3 py-1 cursor-pointer">L</p>
+        </div>
+        <div>
+          <p className="bg-slate-200 px-3 py-1 cursor-pointer">XL</p>
+        </div>
+        <div>
+          <p className="bg-slate-200 px-3 py-1 cursor-pointer">XXL</p>
+        </div>
+      </div>
+    </div>
+    
+    <div className="flex gap-2 mt-2 ">
+      <input type="checkbox" id="bestseller" />
+      <label className="cursor-pointer" htmlFor="bestseller">Add to bestseller</label>
+    </div>
+      {/* <button type="submit" className="w-28 py-3 mt-4 bg-black text-white">ADD</button> */}
+    
+         <button type="submit" className=" cursor-pointer border border-zinc-300 text-white bg-zinc-900 hover:bg-white hover:text-black text-xs font-semibold  tracking-wider px-4 py-2  transition-colors duration-700 ">
+        ADD
+      </button>
+     
+   </form>
   )
 }
 
